4a621104abc30705f405d04aae732507ab2b37e8
freindst <yiming.liao@cuw.edu>
2016-02-22 19:01:53 -0600

    update
    webforms create

--- routes/webforms.js
+++ routes/webforms.js
@@ -14,15 +14,25 @@ router.get('/', function(req, res) {
 });
 
 router.get('/lists/:Semester_id', function(req, res) {
+	var Semester_id = req.params.Semester_id;
 	res.render('webforms/list', {
-		title: 'Webform List'
+		title: 'Webform List',
+		Semester_id: Semester_id
 	});
 });
 
-router.get('/interviews', function(req, res) {
-	res.render('webforms/interviews/create', {
-		title: "interviews"
-	});
+router.get('/interviews/:Semester_id', function(req, res) {
+	var query = "SELECT Semesters.Semester_id, Students.Student_number, Students.First_name, Students.Last_name, Semesters.Year, Semesters.Season, Semesters.Term, Semesters.Level, Semesters.Section FROM Semesters INNER JOIN Students ON Semesters.Student_id=Students.Student_id WHERE Semesters.Semester_id = ?"
+	connection.query(query, [req.params.Semester_id], function(err, results) {
+		if (err) throw err;
+
+		res.render('webforms/interviews/create', {
+			title: "Interviews",
+			result: results[0]
+		});
+
+	})
+
 });
 
 module.exports = router;
