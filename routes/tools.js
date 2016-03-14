f7b0826d56767e9780dd21baff0b59b8d90ea0b8
blade <yiming.liao@cuw.edu>
2016-03-14 16:54:23 -0500

    compact

--- routes/tools.js
+++ routes/tools.js
@@ -11,18 +11,5 @@ module.exports = function() {
 		}
 	};
 
-	this.renderWebformsIndex = function(webformType, req, res) {
-		var query = "SELECT Semesters.Semester_id, Students.Student_number, Students.First_name, Students.Last_name, Semesters.Year, Semesters.Season, Semesters.Term, Semesters.Level, Semesters.Section FROM Semesters INNER JOIN Students ON Semesters.Student_id=Students.Student_id ORDER BY Students.Student_number";
-		connection.query(query, function(err, results) {
-			if (err) throw err;
-
-			res.render('webforms/index', {
-				title: "Choose a student record",
-				rows: results,
-				user: req.user[0],
-				url: "/webforms",
-				webformType: webformType
-			});
-		});
-	};
+	
 };
