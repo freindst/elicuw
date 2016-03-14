ef6ed7e30f28ff64c549501579442a86e6eacd47
blade <yiming.liao@cuw.edu>
2016-03-14 16:53:02 -0500

    user profile page

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+//tools.js contains global functions
+var tools = require('./tools')();
+
+//A Router-level middleware check user's authentication before using it
+router.use(function (req, res, next) {
+  isAuthenticated(req, res, next);
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/:userid', function(req, res) {
-	res.send(req.params.userid);
+	res.render('users/user', {
+		title: 'User Account',
+		user:req.user[0]
+	});
 })
 
 
