0f9fa533a22378f53caf71d122c730d0aff93275
blade <yiming.liao@cuw.edu>
2016-06-02 18:47:52 -0500

    fix user login check logic

--- passport.js
+++ passport.js
@@ -4,40 +4,41 @@ var bcrypt = require('bcrypt-nodejs');
 
 module.exports = function(app) {
 	passport.serializeUser(function(user, done) {
-	  done(null, user);
+		done(null, user);
 	});
 
 	passport.deserializeUser(function(user, done) {
-	  connection.query('SELECT * FROM Users WHERE UserID = ?', [user.UserID], function(err, user) {
-	    if (user) {
-	      done(err, user);  
-	    }
-	  });
+		connection.query('SELECT * FROM Users WHERE UserID = ?', [user.UserID], function(err, user) {
+			if (user) {
+				done(err, user);  
+			}
+		});
 	});
 
 	passport.use(new LocalStrategy(function(username, password, done) {
 		process.nextTick(function() {
-		  connection.query('SELECT * FROM Users WHERE Username = ?', [username], function(err, user) {
-		    if (err) {
-		      return done(err);
-		    }
+			connection.query('SELECT * FROM Users WHERE Username = ?', [username], function(err, user) {
+				if (err) {
+					return done(err);
+				}
 
-		    bcrypt.compare(password, user[0].Password, function(err, res) {
-		    	if (err) throw err;
+				if (user.length == 0) {
+					return done(null, false, {
+						message: 'Incorrect username.'
+					});
+				}
 
-			    if (user.length == 0) {
-			      return done(null, false, {
-			        message: 'Incorrect username.'
-			      });
-			    }
-			    else if (!res) {
-			      return done(null, false, {
-			      	message: 'Invalid password.'
-			      });
-			    }
-			    return done(null, user[0]);
-		    });
-		  });			
+				bcrypt.compare(password, user[0].Password, function(err, res) {
+					if (err) throw err;
+
+					if ((user.length != 0) && (!res)) {
+						return done(null, false, {
+							message: 'Invalid password.'
+						});
+					}
+					return done(null, user[0]);
+				});
+			});			
 		});
 	}));	
 };
